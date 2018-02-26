ce92465344178c4e6a9c184b2d020638c4b3141a
= <=>
2018-02-26 23:05:29 +0200

    asdfasdf

--- app/index.js
+++ app/index.js
@@ -1,3 +1,7 @@
+import $ from 'jquery'
+import Rx from 'rxjs'
+
+
 /**
  * Application entry point
  */
@@ -8,3 +12,25 @@ import 'styles/index.scss';
 // ================================
 // START YOUR APP HERE
 // ================================
+
+
+
+(() => {
+  const $btn = $('.my-btn')
+  const $input = $('.my-input')
+  const $par = $('.my-par')
+
+  $par.css({border: '2px solid black', backgroundColor: '#999999'})
+
+  const btnS = Rx.Observable.fromEvent($btn, 'click')
+  .subscribe((ev) => $par.animate({
+    color: '#009922',
+    opacity: .3,
+    fontWeight: 800,
+    height: '300px',
+    backgroundColor: '#993456'
+  }, 2000, () => {
+    console.log('completed')
+  }))
+
+})()
